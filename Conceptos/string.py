bbabdf4731df7f882e76dd0617df733f81895102
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-02-07 16:57:58 -0300

    métodos string

--- Conceptos/string.py
+++ Conceptos/string.py
@@ -17,3 +17,35 @@ mensaje2 = mensaje1.capitalize()
 print(f"Mensaje1: {mensaje1}, id: {hex(id(mensaje1))}") #id() => PAra saber la dirección de memoria a la que pertenece.
 print(f"Mensaje2: {mensaje2}, id: {hex(id(mensaje2))}") 
 
+#Método join
+
+tupla_str = ("hola","mundo", "como","estas")
+mensaje = " ".join(tupla_str)
+print(f"Mensaje: {mensaje}")
+
+lista_cursos = ["Java","Python", "Angular", "Spring"]
+mensaje = ", ".join(lista_cursos)
+
+print(lista_cursos)
+
+cadena = "HolaMundo"
+mensaje = ".".join(cadena)
+print(f"Mensaje: {mensaje}")
+
+diccionario = {"Nombre":"juan", "apellido":"Perez","edad":"18"}
+
+llaves = "-".join(diccionario.keys())
+valores = "-".join(diccionario.values())
+
+print(f"Llaves:{llaves} , type:{type(llaves)}")
+print(f"valores:{valores} , type: {type(valores)}")
+
+#Método split = dividir una cadena delimitada en subcadenas
+
+cursos = "Java Python Javascript Angular Spring Excel"
+lista_cursos = cursos.split()
+print(f"Lista cursos: {lista_cursos}")
+
+cursos = "Java , Python , Javascript , Angular , Spring , Excel"
+lista_cursos = cursos.split(" , ",3)#=> con el segundo parámetro los primeros tres elementos se separan y el resto queda en una cadena.
+print(f"Lista cursos: {lista_cursos}")
