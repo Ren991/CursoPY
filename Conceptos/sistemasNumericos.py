fe25c6bbb350b16a2022acc49f806fa49a45eeaf
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-02-07 10:51:30 -0300

    sistemas numéricos

---
+++ Conceptos/sistemasNumericos.py
@@ -0,0 +1,13 @@
+# Existen diferentes sistemas de numeracion
+# Binario-Octal-Decimal-Hexadecimal
+
+#Por defecto está en el sistema decimal
+a = 10
+#binario
+a = 0b1010 #=> 0b (indica que es binario) + 1010 (valor 10 en binario)
+#Octal
+a = 0o12 #=> 0oo (indica que es sistema octal) + 12 (valor 10 en sistema octal)
+#Hexadecimal
+a= 0xA #=> 0x (indica que es sistema hexadecimal) + A (valor 10 en sistema hexadecimal)
+
+print(f"a: {a}")
