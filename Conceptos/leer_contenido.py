e81dd44a4dd02a6ac16eab30c255d1196f464c08
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-02-14 16:25:41 -0300

    mas método de string

--- Conceptos/leer_contenido.py
+++ Conceptos/leer_contenido.py
@@ -17,3 +17,35 @@ with urlopen(peticion) as mensaje:
         for palabra in palabras_por_linea:
             palabras.append(palabra)
 print(palabras)
+
+#Contar ocurrencias cadena 
+cadena ="Hola prueba python cadena"
+print("N° Veces letra a:",cadena.count("a"))
+
+#upper convierte a mayúscula un str
+print(cadena.upper())
+
+#lower convierte a minúscula un str
+print(cadena.lower())
+
+#método para determinar si una cadena se encuentra dentro de otra cadena 
+
+print("Hola" in cadena) #=> Devuelve True si hola está en la variable cadena , de lo contrario devuelve False
+
+# startwith - inicia con
+print(cadena.startswith("Hola")) # retorna bool
+#endswith - termina en 
+print(cadena.endswith("pepe")) # retorna bool
+
+#ALINEAR CADENA
+titulo = "Prueba alinear cadena en python"
+
+print(len(titulo))
+#print(titulo.center(50,"*")) #=> rellena con * hacia ambos lados.
+print(titulo.center(len(titulo)+10,"-"))
+
+#ljust justifica a la izquierda
+print(titulo.ljust(50,"*"))
+
+#rjust justifica a la derecha
+print(titulo.rjust(50,"*"))
