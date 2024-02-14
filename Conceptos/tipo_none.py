dcd8d724d603015c7409d93b12b34d2ff151dd1e
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-02-14 16:41:16 -0300

    Tipo none en python

---
+++ Conceptos/tipo_none.py
@@ -0,0 +1,19 @@
+variable = None 
+
+if variable:
+    print("Se considera verdadera")
+else:
+    print("Se considera falsa") # Tipo None se considera Falso
+    
+'''
+8 Formas de generar un bool False en python
+1. Comilla simple o doble ""/ '' (cadena vacia)
+2. Lista vacía []
+3. Tupla vacía ()
+4. Diccionario vacío {}
+5. Entero => 0 (False), cualquier otro valor se considera True
+6. Float => 0.0
+7. Bool = False
+8. None => False , es clase NoneType
+'''
+    
