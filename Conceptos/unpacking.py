db6926130ada806ef523b22fc0db21d5e18588ca
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-02-15 12:01:37 -0300

    Desempaquetado de Lista-Diccionario y Str

--- Conceptos/unpacking.py
+++ Conceptos/unpacking.py
@@ -28,4 +28,36 @@ x, y = obtener_coordenadas()
 print("Coordenada x:", x)
 print("Coordenada y:", y)
 
+numeros = [1,2,3]
+print(*numeros, sep=" - ")#Desempaquetando cada elemento
 
+def sumar(a,b,c):
+    print(f"Resultado suma: {a+b+c}")
+    
+sumar(*numeros) # => Se desempaqueta el array numeros para que pase como 3 parámetros separados
+
+#Extraer algunas partes de una lista
+mi_lista= [1,2,3,4,5,6]
+a,*b, c, d = mi_lista
+print(a,b,c,d)
+
+#Unir lista
+lista1= [1,2,3]
+lista2 = [4,5,6]
+lista3= [lista1,lista2]
+
+print(f"Lista de listas: {lista3}")
+lista3= [*lista1,*lista2] #Se desempaqueta lista1 y lista2
+print(f"Unica lista: {lista3}")
+
+#Unir diccionarios
+dic1={"A":1,"B":2,"C":3}
+dic2 = {"D":4,"E":5}
+dic3 = {**dic1, **dic2}
+print(f"Unir diccionarios: {dic3}")
+
+#Construir una lista a partir de un str
+
+listaStr = [*"HolaMundo"]
+print(listaStr)
+print(*listaStr)
