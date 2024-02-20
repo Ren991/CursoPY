d1af577b19fc12f89d15306f9d6f40b61baf5713
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-02-20 12:26:55 -0300

    método repr

---
+++ Conceptos/POO_representacion_objetos.py
@@ -0,0 +1,15 @@
+# 3 formas para representar objetos en py (str, repr, format)
+
+class Persona:
+    def __init__(self, nombre, apellido) -> None:
+        self.nombre = nombre
+        self.apellido = apellido
+        
+    # repr , más enfocado a los programadores
+    
+    def __repr__(self) -> str:
+        return f"Persona(nombre:{self.nombre}, apellido:{self.apellido})"
+    
+persona1 = Persona("Juan","Perez")
+print(persona1)
+        
