3df3f35d4e139caa16cd376f3107923629bee02b
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-02-21 15:15:24 -0300

    login y signup

--- FlaskWebApp/website/auth.py
+++ FlaskWebApp/website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint # BluePrint significa que tiene muchas url definidas. 
+from flask import Blueprint, render_template # BluePrint significa que tiene muchas url definidas. 
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-    return "<p>Login</p>"
-
+    #return render_template("login.html", text="Testing", user="Tim") # Así se pasan datos a través de las pantallas.
+    return render_template("login.html", boolean=False)
 @auth.route("/logout")
 def logout():
-    return "<p>logout</p>"
+    return "<p>Logout</p>"
 
 @auth.route("/sign-up")
 def sign_up():
-    return "<p>Sign Up </p>"
+    return render_template("sign_up.html")
