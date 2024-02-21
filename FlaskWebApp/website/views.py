cb086e1fe04ec04b065356ef35ab2632933058ca
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-02-21 11:38:25 -0300

    navbar homepage , template jinja

--- FlaskWebApp/website/views.py
+++ FlaskWebApp/website/views.py
@@ -1,11 +1,11 @@
 '''Todas las rutas que no sean auth
 van acá
 '''
-from flask import Blueprint # BluePrint significa que tiene muchas url definidas. 
+from flask import Blueprint, render_template # BluePrint significa que tiene muchas url definidas. 
 
 views = Blueprint("views", __name__)
 
 @views.route("/")
 def home(): #Esta función se ejecuta siempre que vayamos a la ruta mencionada arriba
-    return "<h1>Test</h1>"
+    return render_template("home.html")
 
