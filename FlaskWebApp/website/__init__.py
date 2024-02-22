6f37c4fa2d811522b8a5f89ce0eda5805365234f
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-02-22 09:22:40 -0300

    sqlalchemy setup

--- FlaskWebApp/website/__init__.py
+++ FlaskWebApp/website/__init__.py
@@ -1,10 +1,16 @@
 # Hace que la carpeta sea un python package.
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy # SE usa para la DB
+
+db = SQLAlchemy
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__) # Representa el nombre del archivo
     app.config["SECRET_KEY"] = "clave_prueba_app" # Encripta y asegura la sesion 
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
     
     from .views import views
     from .auth import auth
