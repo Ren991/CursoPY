2656478f5e6d1210612ecf7dcd8fcae392f80b3c
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-02-26 16:46:31 -0300

    Pantalla de botones con grid

--- tkinter/grid.py
+++ tkinter/grid.py
@@ -6,6 +6,13 @@ ventana = tk.Tk()
 ventana.geometry("600x400")
 ventana.title("Manejo de grid")
 
+# Configuración del grid
+ventana.rowconfigure(0, weight=1)
+ventana.rowconfigure(1,weight=10)
+ventana.columnconfigure(0, weight=1)
+ventana.columnconfigure(1, weight=5)
+
+
 #métodos de los eventos
 
 def evento1():
@@ -16,8 +23,19 @@ def evento2():
 
 # Definimos 2 botones
 boton1 = ttk.Button(ventana, text="Botón 1", command=evento1)
-boton1.grid(row=0, column=0)
+boton1.grid(row=0, column=0, sticky="NSWE")
 
+# N( arriba), E(derecha) , S(abajo), W(izquierda)
 boton2= ttk.Button(ventana, text="Boton 2", command=evento2)
-boton2.grid(row=1, column=0)
+boton2.grid(row=1, column=0, sticky="NSWE" )
+
+# Boton3
+boton3 = ttk.Button(ventana, text="boton 3")
+boton3.grid(row=0 , column=1, sticky="NSWE")
+
+# Boton 4
+
+boton4 = ttk.Button(ventana, text="Boton 4")
+boton4.grid(row=1, column=1, sticky="NSWE")
+
 ventana.mainloop()
