3854ec4629a6b07aac326e2c706de64d9adb5934
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-02-27 15:57:43 -0300

    Cambio de color y minsize

--- tkinter/login.py
+++ tkinter/login.py
@@ -1,5 +1,13 @@
 from customtkinter import CTk, CTkFrame
 
+c_negro = "#010101"
+c_morado = "#7f5af0"
+c_verde = "#2cb67d"
+
+
 root = CTk()#Ventana
-root.geometry("500x600+350+20")
+root.geometry("500x600+350+20")# Se establece tamaño de ventana
+root.minsize(480,500)# Se establece tamaño mínimo
+root.config(bg = c_negro)#Fondo negro 
+root.title("Inicio de Sesión")# Se ingresa un título
 root.mainloop()
