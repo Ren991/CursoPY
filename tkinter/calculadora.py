3f47fcd01ef3bbe2b4d0069b17620f67b373afba
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-03-02 19:07:27 -0300

    frame texto calculadora

--- tkinter/calculadora.py
+++ tkinter/calculadora.py
@@ -8,6 +8,29 @@ class Calculadora(tk.Tk):
         self.resizable(0,0)
         self.title("Calculadora")
         
+        # Atributos de clase
+        self.expresion= ""
+        # Caja de texto ( input )
+        self.entrada= None
+        # StringVar lo utilizamos para obtner el valor del input
+        self.entrada_texto = tk.StringVar()
+        # Creamos los componentes
+        self._creacion_componentes()
+    
+    #Métodos de clase
+    #método para crear los componentes    
+    def _creacion_componentes(self):
+        # Creamos un frame para la caja de texto
+        entrada_frame = tk.Frame(self, width=400, height=50, bg="grey")
+        entrada_frame.pack(side=tk.TOP)
+        
+        # Caja de texto
+        entrada = tk.Entry(entrada_frame, font=("arial", 18, "bold"), 
+                           textvariable=self.entrada_texto,width=30, justify=tk.RIGHT)
+        
+        entrada.grid(row=0, column=0,ipady=10)
+                
+        
 
 if __name__ == "__main__":
     calculadora = Calculadora()
