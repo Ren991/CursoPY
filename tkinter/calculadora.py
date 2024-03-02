37056670834fafc68952ce77b34c8c6e017d5a08
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-03-02 19:19:24 -0300

    boton dividir

--- tkinter/calculadora.py
+++ tkinter/calculadora.py
@@ -33,16 +33,25 @@ class Calculadora(tk.Tk):
         botones_frame.pack()
         
         # Primer renglon
+        # Boton limpiar
         boton_limpiar = tk.Button(botones_frame, text="C", width="32",height=3,
                                   bd=0, bg="#eee", cursor="hand2",
                                   command=self._evento_limpiar)
         boton_limpiar.grid(row=0,column=0,columnspan=3,padx=1,pady=1)
+        # Boton dividir
+        boton_dividir = tk.Button(botones_frame, text="/", width=10, height=3, bd=0,
+                                bg="#eee",cursor="hand2",
+                                command=lambda: self._evento_click("/"))
+        boton_dividir.grid(row=0,column=3,padx=1,pady=1)
     
-
     def _evento_limpiar(self):
          self.expresion = ""
          self.entrada_texto.set(self.expresion)
-         
+    
+    def _evento_click(self,elemento):
+        # Concatenamos el nuevo elemento a la expresion ya existente
+        self.expresion = f"{self.expresion}{elemento}"
+        self.entrada_texto.set(self.expresion)
         
                 
         
